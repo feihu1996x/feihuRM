197469e4bf08539d20e8b047dc928e247341301b
feihu1996x <gsx0312@qq.com>
2018-02-11 16:02:58 +0800

    update files

---
+++ README.md
@@ -0,0 +1,11 @@
+## 这是一个微信小程序demo
+
+![welcome](http://www.feihu1996.cn/demo/welcome.png)
+
+![posts](http://www.feihu1996.cn/demo/posts.png)
+
+![posts-detail](http://www.feihu1996.cn/demo/posts-detail.png)
+
+![movies](http://www.feihu1996.cn/demo/movies.png)
+
+![movies-detail](http://www.feihu1996.cn/demo/movies-detail.png)
---
+++ data/posts-data.js
@@ -0,0 +1,121 @@
+var local_database = [
+    {
+        date: "Sep 18 2016",
+        title: "刺客信条",
+        imgSrc: "/images/post/crab.png",
+        avatar: "/images/avatar/1.png",
+        content: "《刺客信条》是由育碧蒙特利尔工作室研发的一款动作冒险类游戏，主要角色有阿泰尔、康纳、艾吉奥，于2007年发行第一部，游戏平台为PS3、PC和PSP",
+        reading: "112",
+        collection: "96",
+        headImgSrc: "/images/post/crab.png",
+        author: "林白衣",
+        dateTime: "24小时前",
+        detail: "Desmond（戴斯蒙）一觉醒来发现自己被一群人给抓了，他们自称是圣殿骑士，并且知道Desmond是刺客，需要通过Desmond的DNA来探索古代一些伊甸园圣器的信息。[2]  于是Desmond就只能被迫进入一台叫Animus的机器，来读取自己祖先留在DNA里的记忆。他读取到了一位刺客大师Altair（阿泰尔）。Altair作为刺客首领Mualim最得意的弟子，被派遣一项任务和两名刺客兄弟去一个古墓中拿取一件宝物，但当Altair到达的时候发现圣殿骑士已经取得了宝物。Altair高估了自己的能力硬碰硬，结果害死了自己的一个兄弟，另一个兄弟夺回了宝物死里逃生。于是回来后Altair被贬为最低等的刺客从头开始学习。同时，师傅Mualim让Altair刺杀九位圣殿骑士首领。Altair一步一步完成任务，但直到杀死最后一位圣殿骑士首领Robert（罗伯特）之后，他才知道原来自己的师傅Mualim是深深隐藏着的第十位圣殿骑士首领，为的就是利用Altair取得宝物，还能干掉自己的同伙。当Altair返回刺客基地马西亚夫城堡的时候，Mualim已经对那件宝物——伊甸园圣器金苹果有所研究，并得到了巨大的力量，但Altair作为正统的刺客，凭自己不屈的毅力杀死了背叛刺客的师傅。回到现代，圣殿骑士们已经对金苹果有所了解，Desmond面临着被处死的危机。同时，他发现在Animus中呆久了之后，学会了Altair的一项独特刺客技能——鹰眼，并通过鹰眼发现了地面、墙壁上的血迹。原来在他之前已经有人被这样当做实验品，但不幸死亡。（那就是16号，Desmond是17号。）",
+        postId: 0,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100003507bR0gDKBm.m4a?fromtag=38",
+            title: "夜夜夜夜-齐秦",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001TEc6V0kjpVC.jpg?max_age=2592000"
+        }
+    },
+    {
+        title: "刺客信条：血缘",
+        content: "本作首次登陆PSP掌机，但并不是一款炒冷饭或者是纯粹的简化移植作品。将拥有独立的故事架构，时代背景设定在紧接一代故事发生后，还可以解释《刺客信条》和《刺客信条2》之间关联的途径。",
+        imgSrc: "/images/post/bl.png",
+        reading: 62,
+        detail: "当时的最强刺客阿泰尔在初代剧情中与同伴在耶路撒冷的所罗门神殿中夺取宝物“伊甸的碎片”。可惜失败独自逃走，在要塞中与师父AlMualim（第十个圣殿骑士，最终关卡）表明任务失败，但同伴之一（Malik）从神殿中逃了出来以弟弟和左手臂的代价夺回了宝物。而阿泰尔因为自身的傲慢和蔑视信条被师父补了一刀，貌似假死后来苏醒，并且剥夺最强刺客称号和装备，阿泰尔恢复身份的任务是杀死9个圣殿骑士，可惜随着任务的推进，他发觉师父同样有着邪恶的野心，于是杀死师父夺回宝物。如今其他地域还有别的宝物存在，为了不被圣殿骑士利用，阿泰尔独自踏上了寻宝的道路。",
+        collection: 92,
+        dateTime: "24小时前",
+        headImgSrc: "/images/post/bl.png",
+        author: "迷的城",
+        date: "Nov 20 2016",
+        avatar: "/images/avatar/1.png",
+        postId: 1,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100003GdCmG4NkEOR.m4a?fromtag=38",
+            title: "鬼迷心窍-李宗盛",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000002xOmp62kqSic.jpg?max_age=2592000"
+        }
+    },
+    {
+        //按住alt + shift + F 可以格式化代码样式
+        title: "刺客信条2",
+        content: "《刺客信条2》延续一代的剧情，玩家将在古今交错的虚拟与真实世界中穿梭，化身为无声夺命的高强刺客，在文艺复兴时期的意大利展开冒险。《刺客信条2》将引入全新的任务和游戏要素，增加更多种类的武器和道具并借此衍生出更加丰富的玩法。",
+        detail: "本次《刺客信条2》的背景设定在1486年的意大利文艺复兴时代 ，地点在威尼斯、佛罗伦萨以及托斯卡纳。玩家依旧是操作戴西蒙德的祖先，本次的祖先名叫艾吉奥。艾吉奥出身贵族，但是没多久他就被卷入了他人对他家族的阴谋中，于是他决定找阴谋策划者复仇。在艾吉奥的任务中，他将要面对强大的Grimaldi家族，这个家族也就相当于今天黑手党的前身。除了要对抗Grimaldi家族的成员，我们知道圣殿骑士团也会回来。作为敌方重要角色的Carno Grimaldi，在威尼斯当地可谓有权有势。很显然，这个家伙身边保护重重，而他对自己过于自信也是他失败的原因 。",
+        imgSrc: "/images/post/sls.jpg",
+        headImgSrc: "/images/post/sls.jpg",
+        reading: 62,
+        collection: 92,
+        author: "知乎",
+        date: "Nov 12 2016",
+        dateTime: "三天前",
+        avatar: "/images/avatar/3.png",
+        postId: 2,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100004HLusI2lLjZy.m4a?fromtag=38",
+            title: "女儿情-万晓利",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000004Wv5BO30pPc0.jpg?max_age=2592000"
+        }
+    },
+    {
+        title: "刺客信条2：探索",
+        content: "《刺客信条2：探索》是由Ubisoft发行的一款动作冒险类游戏，于2010年2月2日发行。游戏的剧情发生在《刺客信条II》故事的15年之后，（前情参见词条：刺客信条II ）主人公也是II的主角艾吉奥·奥迪托雷·达·佛罗伦萨（Ezio Auditore da Firenze）。",
+        imgSrc: "/images/post/xiaolong.jpg",
+        reading: 102,
+        detail: "游戏的剧情发生在《刺客信条II》故事的15年之后，（前情参见词条：刺客信条II ）主人公也是II的主角艾吉奥·奥迪托雷·达·佛罗伦萨（Ezio Auditore da Firenze）。当他来到15世纪的西班牙，去解放那些被西班牙宗教法庭的幌子下监禁的跟随他的刺客们。在这个过程中，埃齐奥发现圣殿骑士团计划向西航行去发现新大陆。埃齐奥必须救下克里斯托弗·哥伦布(Christoffa Corombo)，并且杀死托马斯·托尔克马达以消除圣殿骑士的威胁。 埃齐奥也不得不暗杀其他一些重要的目标，如：加斯珀·马丁内斯，佩德罗·略伦特，圣殿骑士的间谍以及胡安·德·马里罗。",
+        collection: 92,
+        dateTime: "24小时前",
+        headImgSrc: "/images/post/xiaolong.jpg",
+        author: "猫是猫的猫",
+        date: "Nov 20 2016",
+        avatar: "/images/avatar/5.png",
+        postId: 3,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100002mWVx72p8Ugp.m4a?fromtag=38",
+            title: "恋恋风尘-老狼",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001VaXQX1Z1Imq.jpg?max_age=2592000",
+        }
+    },
+    {
+        title: "刺客信条：兄弟会",
+        content: "《刺客信条：兄弟会》是继《刺客信条2》之后的延续作品，不过这并不是正统的《刺客信条3》，《刺客信条：兄弟会》的一大特色就是加入了对多人模式的支持。",
+        imgSrc: "/images/post/vr.png",
+        reading: 102,
+        detail: "1499年12月文艺复兴时期，40岁的艾吉奥放弃刺杀教皇亚历山大六世罗德里格·波齐亚（圣殿骑士团大团长）后回到蒙特里久尼。1500年1月，教宗之子西泽尔·波奇亚一天晚上领兵对城市发动攻击。西泽尔于城破之时夺回艾齐奥伯父马里奥·奥迪托雷保管的古代遗物“伊甸园碎片-金苹果”、将之枪杀并命士兵枪伤艾齐奥。艾齐奥负伤带领佣兵及家人逃走后独自前往意大利首都罗马，并发现波奇亚家族已几乎完全控制了整个罗马市，市区百业萧条，市民被剥削而生活困苦。以市内台伯河上的小岛台伯岛为基地，艾齐奥联结市内妓女、盗贼及佣兵势力，以及招揽反抗的市民加入其刺客兄弟会以解放在教宗势力控制下的罗马。于各方协助下，艾齐奥得知西泽尔的财政由其兄长胡安·波奇亚负责，而西泽尔则欲借助法国瓦卢瓦公爵路易十二的军事力量以及发明家达芬奇之战争机器以征服意大利。",
+        collection: 26,
+        dateTime: "24小时前",
+        headImgSrc: "/images/post/vr.png",
+        author: "深白色",
+        date: "Nov 20 2016",
+        avatar: "../../../images/avatar/3.png",
+        postId: 4,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100000Zn0vS4fKKo8.m4a?fromtag=38",
+            title: "沉默是金-张国荣",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000003at0mJ2YrR2H.jpg?max_age=2592000"
+        }
+    },
+    {
+        title: "刺客信条：启示录",
+        content: "《刺客信条：启示录》是法国育碧（Ubisoft）公司推出的3D动作冒险系列游戏的刺客信条系列第四部作品。该作品由育碧蒙特利尔制作室领导开发，加之安纳西、Massive、魁北克、新加坡布加勒斯特制作室的合力协作。",
+        imgSrc: "/images/iqiyi.png",
+        reading: 96,
+        detail: "在刺客大师约瑟夫·塔奇姆和刚刚认识的博学女孩索菲亚萨托的帮助下，Ezio逐渐在城中安家落户，并抓紧时间与圣殿骑士赛跑寻找马西亚夫之钥。Ezio还答应了奥斯曼帝国王子，因权力之争丧父的苏莱曼王子（Suleiman，后来的苏莱曼大帝），帮助他调查圣殿骑士的指挥官是何人和大本营所在。\n在苏莱曼的请求下，艾吉奥刺杀了疑似圣殿骑士的近卫队长，但他发现近卫队长竟一直与圣殿骑士斗争，并在临死之时交给艾吉奥一封信。艾吉奥便突破了金角湾的封锁乘船离开了君士坦丁堡。\n当艾吉奥来到圣殿骑士的老巢——卡帕多奇亚后，找到了近卫队长塔里克派来的卧底，并引爆了圣殿骑士的军火库杀死了君士坦丁十一世。当离开时艾吉奥才发现真正的圣殿骑士领导人是苏莱曼的叔叔艾哈迈德，他威胁艾吉奥交出马西亚夫之钥，否则他会杀死索菲亚。",
+        collection: 26,
+        dateTime: "21小时前",
+        headImgSrc: "/images/iqiyi.png",
+        author: "深白色",
+        date: "Nov 20 2016",
+        avatar: "../../../images/avatar/5.png",
+        postId: 5,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100002I8eGJ28BI17.m4a?fromtag=38",
+            title: "朋友-谭咏麟",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000004eGsCN3SUheO.jpg?max_age=2592000"
+        }
+    },
+]
+
+module.exports = {
+    postList: local_database
+}
